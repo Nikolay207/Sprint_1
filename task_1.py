30df8a3606b9922430eec8d7d983897dde8a0fb8
Nikolay207 <korochkinnickolay@yandex.ru>
2026-06-29 00:17:12 +0300

    Update task_1.py

--- task_1.py
+++ task_1.py
@@ -1,15 +1,1 @@
-a = '1h 45m,360s,25m,30m 120s,2h 60s'
-a_new= a.replace(',', ' ')
-b=a_new.split()
-m=0
-for i in b:
-    if 'h' in i:
-        d=i.replace('h', '')
-        m = m + int(d)*60
-    elif 'm' in i:
-        t=i.replace('m', '')
-        m = m + int(t)
-    elif 's' in i:
-        t = i.replace('s', '')
-        m = m + int(t)//60
-print(m)
+
