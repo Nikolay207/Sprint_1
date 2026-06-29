342295021deb34f9b37b48cf6547b466496b2477
Korochkin Nikolay <korochkinnickolay@yandex.ru>
2026-06-29 04:41:58 +0300

    Выполнение задания

---
+++ task_5.py
@@ -0,0 +1,43 @@
+class TestCase:
+
+    def __init__(self):
+        self.steps = {}
+        self.result = None
+
+    def set_step(self,step_number,step_text):
+        self.steps[step_number]= step_text
+        print(self.steps)
+
+    def delete_step(self,step_number):
+        self.steps.pop(step_number)
+        print(self.steps)
+
+    def set_result(self,result):
+        self.result = result
+        print(result)
+
+    def get_test_case(self):
+        print('{')
+        print("     Шаги: {")
+        for key, value in self.steps.items():
+            print("         "+str(key)+': '+str(value)+ ',')
+        print('     },')
+        print("     Ожидаемый результат: " + str(self.result))
+        print('}')
+
+
+test_case_1 = TestCase()
+test_case_1.set_step(1, 'Перейти на сайт')
+test_case_1.set_step(3, 'Перейти в раздел Товары')
+test_case_1.delete_step(3)
+test_case_1.set_step(2, 'Перейти в раздел Товары')
+test_case_1.set_step(3, 'Нажать кнопку «В корзину» у первого товара')
+test_case_1.set_result('Товар окажется в корзине')
+test_case_1.get_test_case()
+
+test_case_2 = TestCase()
+test_case_2.set_step(1, 'Перейти на сайт')
+test_case_2.set_step(2, 'Перейти в раздел Корзина')
+test_case_2.set_step(3, 'Нажать кнопку "Удалить"')
+test_case_2.set_result('Товар удален из корзины')
+test_case_2.get_test_case()
